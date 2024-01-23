06ec7467f2bf84bc4e3b80ca535a4b98f2d4b895
unknown <ricardo41costa@gmail.com>
2024-01-23 14:23:25 +0000

    UI stuff

--- src/ui/screen/Credits/Credits.gd
+++
@@ -1,7 +0,0 @@
-class_name Credits
-extends Control
-
-func _input(event):
-	pass
-	#if event is InputEventKey or event is InputEventMouseButton:
-	#	SceneManager.change_scene(self, "res://src/scene/MainScene.tscn")
